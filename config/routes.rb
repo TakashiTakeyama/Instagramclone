1b135a1c5e15ecf5229ee3968fc0ae7cb7b4f7b9
Takashi <Takashi@gmail.com>
2019-07-16 12:14:46 +0900

    usersコントローラーのshowアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only:[:new]
+  resources :users
   resources :pictures
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
