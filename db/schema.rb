0fa3114d4ad84eb9945c2c058fa5bb8c7304d3cb
Takashi <Takashi@gmail.com>
2019-07-16 14:26:30 +0900

    usersテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_112114) do
+ActiveRecord::Schema.define(version: 2019_07_16_052316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_112114) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
   end
 
 end
