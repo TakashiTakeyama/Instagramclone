54bd6c4412fb623f2eeed123703b18979799e268
Takashi <Takashi@gmail.com>
2019-07-17 11:44:13 +0900

    詳細ページから投稿者以外のuserには編集と削除ボタンが表示されないようにした

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-    @pictures = Picture.all
+      @pictures = Picture.all
   end
 
   # GET /pictures/1
@@ -15,11 +15,20 @@ class PicturesController < ApplicationController
 
   # GET /pictures/new
   def new
-    @picture = Picture.new
+    if logged_in?.present?
+      @picture = Picture.new
+    else
+      redirect_to new_session_path, notice: "ログインしてください"
+    end
   end
 
   # GET /pictures/1/edit
   def edit
+    # @picture = current_user.pictures.build(picture_params)
+    if session[:user_id] == @picture.user_id
+    else
+      redirect_to pictures_path, notice:"自分が投稿した記事以外は編集できません"
+    end
   end
 
   def confirm
@@ -68,11 +77,16 @@ class PicturesController < ApplicationController
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
+    if session[:user_id] == @picture.user_id
     @picture.destroy
     respond_to do |format|
       format.html { redirect_to pictures_url, notice: '記事を削除しました。' }
       format.json { head :no_content }
     end
+    else
+      redirect_to pictures_path, notice:"自分が投稿した記事以外は削除できません"
+    end
+
   end
 
   private
